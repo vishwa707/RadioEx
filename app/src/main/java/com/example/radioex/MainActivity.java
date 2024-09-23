a962594c9c2231f54c39d384a9d6452b94bc775c
Vishwa Tej <ixsaac707@gmail.com>
2024-09-23 19:09:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/radioex/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.radioex;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this, "Selected: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
